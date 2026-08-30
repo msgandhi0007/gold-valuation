01239f67e9354ef9ecdb2b692f582b98cf307966
msgandhi0007 <msgandhi000@gmail.com>
2026-08-30 22:07:30 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
-const CACHE='gold-valuation-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='rajendra-gold-valuation-v22';
+const CORE=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached=>{
+      const network=fetch(event.request).then(res=>{
+        if(res && res.ok){
+          const copy=res.clone();
+          caches.open(CACHE).then(c=>c.put(event.request,copy));
+        }
+        return res;
+      }).catch(()=>cached);
+      return cached || network;
+    })
+  );
+});
